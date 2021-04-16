39abce5e5a6cbd4fa7cd9d7ad599d3f53a14874a
GitShark <53007941+Nikolai-Aleksandrovich@users.noreply.github.com>
2021-04-16 17:30:22 +0800

    update

---
+++ src/main/java/com/calculator/configuration/RouterFunctionConfig.java
@@ -0,0 +1,13 @@
+package com.calculator.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Yuyuan Huang
+ * @create 2021-04-13 16:32
+ */
+@Configuration
+public class RouterFunctionConfig {
+    @Bean
+}
---
+++ src/main/java/com/calculator/controller/HomeController.java
@@ -0,0 +1,9 @@
+package com.calculator.controller;
+
+/**
+ * @author Yuyuan Huang
+ * @create 2021-04-13 16:27
+ */
+
+public class HomeController {
+}
